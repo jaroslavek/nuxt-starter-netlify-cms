91c8087aa16341781cd3435d0364e472c987b334
jaroslavek <jaroslav.horalek@gmail.com>
2023-02-09 10:03:15 +0100

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,7 @@
   <div
     class="px-8 mx-auto mt-12 prose sm:px-6 md:px-4 lg:px-2 xl:px-0 xl:prose-xl lg:prose-lg md:prose-md"
   >
-    <h1>{{ page.title }}</h1>
+    <h1>{{ page.title }} Jho edit 1.0</h1>
     <nuxt-content :document="page" />
   </div>
 </template>
